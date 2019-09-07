2413130f8ea4a549a30a9e2388ec848c0601c372
David Moates <david.m.moates@gmail.com>
2019-09-07 16:37:23 -0500

    did mouseover, keydown, and wheel

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-// Your code goes here
+// Selectors
+const funBusH1 = document.querySelector('.logo-heading');
+const theBody = document.querySelector('body');
+const img = document.querySelectorAll('img');
+
+funBusH1.addEventListener('mouseover', () => {
+  funBusH1.style.fontSize = '5.7rem';
+  setTimeout(() => {
+    funBusH1.style.fontSize = '';
+  }, 500);
+}, false);
+
+theBody.addEventListener('keydown', () => {
+  let x = Math.floor(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
+  const rbgColor = 'rgb(' + x + ',' + y + ',' + z + ')';
+  theBody.style.background = rbgColor;
+});
+
+let scale = 1;
+for (let i = 0; i < img.length; i++) {
+  img[i].addEventListener('wheel', (eventZoom) => {
+    eventZoom.preventDefault();
+    scale += eventZoom.deltaY * -0.01;
+// Restrict Scale
+    scale = Math.min(Math.max(.125, scale), 4);
+// Apply Scale Transform
+    img[i].style.transform = `scale(${scale})`;
+  });
+};
